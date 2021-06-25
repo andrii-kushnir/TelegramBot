d841442bfd1ccf7ea14f0d9a252f262ecac58679
andrii-kushnir <andrijk777@gmail.com>
2021-06-25 17:44:07 +0300

    Add Telegram API

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,25 +10,46 @@ using Telegram.Bot.Args;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
+using TeleSharp.TL;
+using TLSharp.Core;
 
 namespace TelegramBot
 {
     class Program
     {
         //бот Арсеній
-        private const int apiId = 1805032362;
-        private const string apiHash = "AAEmfvbU4lxx4WDCtwUhaVPofbg8vKa5QDI";
+        //private const int apiId = 1805032362;
+        //private const string apiHash = "AAEmfvbU4lxx4WDCtwUhaVPofbg8vKa5QDI";
 
         //бот АндрійБот
         //private const int apiId = 1802156713;
         //private const string apiHash = "AAHA1ZNEMBW94UHppMMU6RrMP-nxjWuQsXw";
 
+        //AndriiBot
+        private const int apiId = 6423473;
+        private const string apiHash = "37e7866209e483e9f35edb45e96950b1";
+
+        //Test configuration:149.154.167.40:443
+        //Production configuration:149.154.167.50:443
+
+
+
         private static TelegramBotClient _botClient;
         private static readonly List<Message> _messages = new List<Message>();
 
+        private static TelegramClient _botApi;
+
         static void Main(string[] args)
         {
-            Do_Bot();
+            //Do_Bot();
+
+            var phone = "380689559241";
+            Do_TelegramAPI(phone).Wait();
+            AuthenticationAsync(phone).Wait();
+
+            SendMessage("380676722619", "Вперьод!").Wait();
+
+            Console.ReadLine();
         }
 
         [Obsolete]
@@ -104,32 +126,46 @@ namespace TelegramBot
         }
 
 
-        //static void Do(int apiId, string apiHash)
-        //{
-        //    _client = new TelegramClient(apiId, apiHash);
-        //    _client.ConnectAsync().Wait();
-        //}
+        static async Task Do_TelegramAPI(string phone)
+        {
+            var token = $"{apiId}:{apiHash}";
 
-        //static void Authentication(string phone)
-        //{
-        //    var hash = _client.SendCodeRequestAsync(phone).Result;
-        //    var code = "<code_from_telegram>"; // you can change code in debugger
+            var store = new FileSessionStore(new DirectoryInfo(@"C:\Users\Andrii.Kushnir\source\repos\TelegramBot\bin\Debug"));
 
-        //    var user = _client.MakeAuthAsync(phone, hash, code).Result;
-        //}
+            _botApi = new TelegramClient(apiId, apiHash, store, phone);
+            await _botApi.ConnectAsync();
+        }
 
-        //static async Task SendMessage(string phone, string message)
-        //{
-        //    var result = await _client.GetContactsAsync();
+        static async Task AuthenticationAsync(string phone)
+        {
+            var hash = await _botApi.SendCodeRequestAsync(phone); // запам"ятати цей код!!!!!!!!!!!!!!!!!!!!!!!!
 
-        //    //find recipient in contacts
-        //    var user = result.Users
-        //        .Where(x => x.GetType() == typeof(TLUser))
-        //        .Cast<TLUser>()
-        //        .FirstOrDefault(x => x.Phone == phone);
+            var code = "72592"; // you can change code in debugger
 
-        //    //send message
-        //    await _client.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, message);
-        //}
+            var user = await _botApi.MakeAuthAsync(phone, hash, code);
+        }
+
+        static async Task SendMessage(string phone, string message)
+        {
+            var result = await _botApi.GetContactsAsync();
+
+            var user = result.Users
+                .Where(x => x.GetType() == typeof(TLUser))
+                .Cast<TLUser>()
+                .FirstOrDefault(x => x.Phone == phone);
+
+            if (user == null)
+            {
+                var list = new List<TLInputPhoneContact> {new TLInputPhoneContact() { Phone = phone, FirstName = "Саша", LastName = "Третьяков", ClientId = 0}};
+                await _botApi.ImportContactsAsync(list);
+                result = await _botApi.GetContactsAsync();
+                user = result.Users
+                            .Where(x => x.GetType() == typeof(TLUser))
+                            .Cast<TLUser>()
+                            .FirstOrDefault(x => x.Phone == phone);
+            }
+
+            await _botApi.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, message);
+        }
     }
 }
---
+++ td/format.ps1
@@ -0,0 +1,3 @@
+﻿./src.ps1 | Select-String -NotMatch "CxCli.h" | Select-String -NotMatch "DotNet" | ForEach-Object {
+  clang-format -verbose -style=file -i $_
+}
