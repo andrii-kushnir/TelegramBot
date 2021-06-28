a61ab9e4391ad5494dae875dfcca9111485951de
andrii-kushnir <andrijk777@gmail.com>
2021-06-28 11:53:27 +0300

    Add sendMesssge from phone and to channel

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using TeleSharp.TL;
+using TeleSharp.TL.Messages;
 using TLSharp.Core;
 
 namespace TelegramBot
@@ -25,9 +26,12 @@ namespace TelegramBot
         //private const int apiId = 1802156713;
         //private const string apiHash = "AAHA1ZNEMBW94UHppMMU6RrMP-nxjWuQsXw";
 
-        //AndriiBot
+        //Andrii_My_API
+        private const string sessionPath = @"C:\Users\Andrii.Kushnir\source\repos\TelegramBot\bin\Debug\";
         private const int apiId = 6423473;
         private const string apiHash = "37e7866209e483e9f35edb45e96950b1";
+        private const string phone = "380689559241";
+        private static string code = "46138"; 
 
         //Test configuration:149.154.167.40:443
         //Production configuration:149.154.167.50:443
@@ -41,15 +45,21 @@ namespace TelegramBot
 
         static void Main(string[] args)
         {
+            //var token = $"{apiId}:{apiHash}";
             //Do_Bot();
 
-            var phone = "380689559241";
-            Do_TelegramAPI(phone).Wait();
-            AuthenticationAsync(phone).Wait();
+            Do_TelegramAPI().Wait();
+            if (!_botApi.IsUserAuthorized())
+            {
+                AuthenticationAsync().Wait();
+            }
 
-            SendMessage("380676722619", "Вперьод!").Wait();
+            //SendMessage("380676722619", "Вперьод!").Wait();  //Третьяков
+            //SendMessage("380930418206", "По чому помідори?").Wait(); 
+            SendMessageToChannel("Група Арсенія", "Не хвилюйтесь, це тест").Wait();
 
             Console.ReadLine();
+            _botApi.Dispose();
         }
 
         [Obsolete]
@@ -126,46 +136,107 @@ namespace TelegramBot
         }
 
 
-        static async Task Do_TelegramAPI(string phone)
+        static async Task Do_TelegramAPI()
         {
-            var token = $"{apiId}:{apiHash}";
-
-            var store = new FileSessionStore(new DirectoryInfo(@"C:\Users\Andrii.Kushnir\source\repos\TelegramBot\bin\Debug"));
-
-            _botApi = new TelegramClient(apiId, apiHash, store, phone);
+            _botApi = new TelegramClient(apiId, apiHash, null, sessionPath + phone);
             await _botApi.ConnectAsync();
         }
 
-        static async Task AuthenticationAsync(string phone)
+        static async Task AuthenticationAsync()
         {
-            var hash = await _botApi.SendCodeRequestAsync(phone); // запам"ятати цей код!!!!!!!!!!!!!!!!!!!!!!!!
+            var hash = await _botApi.SendCodeRequestAsync(phone); // запам"ятати код і прописати в константу code!
 
-            var code = "72592"; // you can change code in debugger
-
-            var user = await _botApi.MakeAuthAsync(phone, hash, code);
+            if (String.IsNullOrWhiteSpace(code))
+            {
+                code = Console.ReadLine();
+            }
+            try
+            {
+                await _botApi.MakeAuthAsync(phone, hash, code);
+            }
+            catch (Exception)
+            {
+                code = Console.ReadLine();
+                await _botApi.MakeAuthAsync(phone, hash, code);
+            }
         }
 
         static async Task SendMessage(string phone, string message)
         {
-            var result = await _botApi.GetContactsAsync();
+            var user = await GetUser(phone);
+            if (user != null)
+            {
+                await _botApi.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, message);
+            }
+            else
+            {
+                Console.WriteLine($"Користувач з телефоном {phone} не знайдений");
+            }
+        }
 
-            var user = result.Users
+        static async Task<TLUser> GetUser(string phone)
+        {
+            var users = await _botApi.GetContactsAsync();
+
+            var user = users.Users
                 .Where(x => x.GetType() == typeof(TLUser))
                 .Cast<TLUser>()
                 .FirstOrDefault(x => x.Phone == phone);
-
             if (user == null)
             {
-                var list = new List<TLInputPhoneContact> {new TLInputPhoneContact() { Phone = phone, FirstName = "Саша", LastName = "Третьяков", ClientId = 0}};
+                Console.Write("Новий користувач. Введіть ім'я: ");
+                var firstName = Console.ReadLine();
+                Console.Write("Введіть прізвище: ");
+                var lastName = Console.ReadLine();
+                var list = new List<TLInputPhoneContact> { new TLInputPhoneContact() { Phone = phone, FirstName = firstName, LastName = lastName, ClientId = 0 } };
                 await _botApi.ImportContactsAsync(list);
-                result = await _botApi.GetContactsAsync();
-                user = result.Users
+                users = await _botApi.GetContactsAsync();
+                user = users.Users
                             .Where(x => x.GetType() == typeof(TLUser))
                             .Cast<TLUser>()
                             .FirstOrDefault(x => x.Phone == phone);
             }
+            return user;
+        }
 
-            await _botApi.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, message);
+        static async Task SendMessageToChannel(string channel, string message)
+        {
+            var channels = (TLDialogs)await _botApi.GetUserDialogsAsync();
+            if (channels != null)
+            {
+                //var chats = channels.Chats.ToList(); // 3 і більше людей
+                //var dialogs = channels.Dialogs.ToList(); // Chats + Users
+                //var users = channels.Users.ToList(); // розмови 1 на 1
+                //var messages = channels.Messages.ToList(); // останні повідомлення кожної розмови
+
+                var chat = channels.Chats
+                            .Where(x => x.GetType() == typeof(TLChat))
+                            .Cast<TLChat>()
+                            .FirstOrDefault(a => a.Title == channel);
+                if (chat != null)
+                {
+                    await _botApi.SendMessageAsync(new TLInputPeerChat() { ChatId = chat.Id }, message);
+                }
+                else
+                {
+                    Console.WriteLine($"Канал {channel} не знайдений");
+                }
+            }
+        }
+
+        static async Task JoinChannel(string phone, string message)
+        {
+            TLInputPhoneContact canal;
+            var request = new TeleSharp.TL.Channels.TLRequestJoinChannel()
+            {
+                Channel = new TLInputChannel
+                {
+                    //ChannelId = channelInfo.Id,
+                    //AccessHash = (long)channelInfo.AccessHash
+                }
+            };
+
+            var responsjoin = await _botApi.SendRequestAsync<TLUpdates>(request);
         }
     }
 }
